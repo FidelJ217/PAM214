abc04eb1e8284dfc748c333f732d524bf2243232
FidelJ217 <124050344@upq.edu.mx>
2025-09-10 23:21:43 -0600

    3 Avance VideoJavaScritpPrincs

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,10 +1,17 @@
-var username = "Yayoshi";
-let lastname = "Laufey";
+var username = "Yayoshi"; //La variable.
+let lastname = "Laufey";  //Otra "forma" de poner variables.
 
-nameuser = 'anatomia';
+username = 'anatomia';   //Le doy otro valor a la variable username.
 
-const PI = 3.1415;
+const PI = 3.1415;      //Como no cambia su valor, es una constante.
+
 
 console.log(username, lastname)
+
+// camelcase: Escribir la mayúscula por cada palabra.
+
+let NombreDePersona = 'gordon';
+
+
 
 
