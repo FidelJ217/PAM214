3e1c3364d7489d9916c72b5c692325d2ff57cf84
FidelJ217 <124050344@upq.edu.mx>
2025-09-10 18:37:03 -0600

    Avance VideoJavaScritpPrincs
    Avance que hice hasta el minuto 40 (Antes de que se borrara )

---
+++ JSParaPrincipiantes/app.js
@@ -0,0 +1,40 @@
+// Tipos de datos
+"Hello World"    //String
+'Hello World'    //String
+
+
+1000.2           //Númericos 
+-2.3
+
+true             //Boolean 
+false
+
+// array Lista de usuarios
+['joe', ' ryan' ,' martha']
+[1,2,3]
+[true, false, true, false]
+
+// Object, representación de un objeto en la vida real
+
+'Ryan'
+70.4
+true
+
+//Llaves que almacenan los objetos. Tipos de datos de usuarios.
+
+{
+    "username": 'Ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true,
+    "friends": ['', '', '']
+}
+
+{
+    "username": 'Megaman',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional": false,
+    "friends": ['', '', '']
+}
+
