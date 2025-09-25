1b3ae48db7f0cf9a792665ab2170f1234a659747
FidelJ217 <124050344@upq.edu.mx>
2025-09-25 17:19:50 -0600

    Práctica 3 Ejercicio c
    Usar async y await con setTimeout para hacer esperar a las promesas declaradas.

--- Practica 3/main.js
+++ Practica 3/main.js
@@ -27,3 +27,23 @@ verificarUsuario("admin")
 verificarUsuario("Ivan")
 .then(res => console.log(res))
 .catch(err => console.log(err)); 
+
+
+
+//EJERCICIO C
+
+function simularPeticionAPI(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve ("Datos recibidos correctamente");
+        }, 5000);
+    });
+}
+
+async function obtenerDatos() {
+    const result = await simularPeticionAPI();
+    console.log(result);
+    
+}
+
+obtenerDatos();
