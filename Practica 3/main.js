3a3bebbd3e730c83a9e3c6a8380809885912c9d7
FidelJ217 <124050344@upq.edu.mx>
2025-09-25 17:04:38 -0600

    Práctica 3 Ejercicio b
    Hacer una promesa que sirva para poder tener los resultados de verificar usuario, y si es admin lo admita y si no lo rechace

--- Practica 3/main.js
+++ Practica 3/main.js
@@ -3,4 +3,27 @@ import { restar } from "./utils.js";
 console.log(restar(6,5));
 
 
+//Ejercicio b:
 
+function verificarUsuario(usuario){
+
+    return new Promise((resolve, reject)=> {
+
+     if ( usuario === "admin"){
+        resolve ("Acceso concedido " + usuario)
+     } else
+        reject ("Acceso denegado " + usuario)
+        
+
+    });
+ 
+}
+
+verificarUsuario("admin")
+.then(res => console.log(res))
+.catch(err => console.log(err));
+
+
+verificarUsuario("Ivan")
+.then(res => console.log(res))
+.catch(err => console.log(err)); 
