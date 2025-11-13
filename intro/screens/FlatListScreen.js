13b298df25d94622c46b470d6313ed4d3727135c
FidelJ217 <124050344@upq.edu.mx>
2025-11-12 18:35:07 -0600

    Práctica 13 List's
    Práctica 13 donde se trabajaron las sectionlist y las flatlist

---
+++ intro/screens/FlatListScreen.js
@@ -0,0 +1,124 @@
+import { Text, StyleSheet, View, FlatList, SectionList } from 'react-native';
+import React, { useState } from 'react';
+
+
+export default function FlatListScreen() {
+
+  const estudiantes =
+    [
+      { id: "1", nombre: "Fidel" },
+      { id: "2", nombre: "Alina" },
+      { id: "3", nombre: "Mildred" },
+      { id: "4", nombre: "Simbad" },
+    ];
+
+  const materias = [
+    {
+      titulo: "Matematicas",
+      data: ["Fidel", "Alina"]
+    },
+
+    {
+      titulo: "Historia",
+      data: ["Mildred", "Simbad"]
+    },
+
+    {
+      titulo: "Programación",
+      data: ["Fidel", "Alina", "Mildred"]
+    },
+  ]
+  return (
+    <View style={styles.container} >
+      <Text style={styles.titulo} >FlatList </Text>
+
+      <FlatList
+        data={estudiantes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text style={styles.item} > {item.nombre}  </Text>
+
+        )}
+        ListHeaderComponent={() => (
+          <Text style={styles.headerFooter} > Inicio de la lista de estudiantes </Text>
+        )}
+
+        ListFooterComponent={() => (
+          <Text style={styles.headerFooter} > Fin de la lista de estudiantes </Text>
+        )}
+      >
+
+      </FlatList>
+      <Text style={styles.titulo} >SectionListt </Text>
+      <SectionList
+
+        sections={materias}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) =>
+          <Text style={styles.item}>-{item} </Text>
+        }
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionHeader}>{section.titulo}</Text>
+        )}
+
+        stickySectionHeadersEnabled
+
+        ItemSeparatorComponent={() => (
+          <View style={styles.Separator} ></View>
+        )}
+        SectionSeparatorComponent={() => (
+          <View style={styles.sectionSeparator}></View>
+        )}
+
+
+
+
+      ></SectionList>
+
+
+
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#F5F5F5"
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  item: {
+    fontSize: 16,
+    backgroundColor: "#ffffff",
+    padding: 10,
+    marginVertical: 4,
+    borderRadius: 8
+  },
+  sectionHeader: {
+    fontSize: 18,
+    fontWeight: "bold",
+    backgroundColor: "#E0E0E0",
+    padding: 8,
+    marginTop: 10,
+    borderRadius: 8
+  },
+  separator: {
+    height: 8,
+  },
+  sectionSeparator: {
+    height: 15,
+  },
+  headerFooter: {
+    textAlign: 'center',
+    fontSize: 15,
+    fontStyle: 'italic',
+    color: '#555',
+    marginVertical: 6,
+  },
+});
